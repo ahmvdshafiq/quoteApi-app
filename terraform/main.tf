ea77c0553ab0f91f6adfde4a62496406d36bb2f6
ahmvdshafiq <ahmvdshafiq@gmail.com>
2025-07-24 23:07:19 +0800

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "quoteaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_container_registry.acr
+  ]
+}
+
+resource "azurerm_role_assignment" "aks_to_acr" {
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  default = "quoteapp-rg"
+}
+
+variable "aks_cluster_name" {
+  default = "quote-aks-cluster"
+}
+
+variable "acr_name" {
+  default = "quoteacrregistry"
+}
